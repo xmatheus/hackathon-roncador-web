4e971f3efd6d03ef552fc08dd866c2e0c2041681
xmatheus <matheuscorreia559@gmail.com>
2020-12-13 14:59:31 -0400

    fix scroll bug

--- components/Fornecedor/style.js
+++ components/Fornecedor/style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
+  overflow-y: auto;
 `;
 
 export const Wrapper = styled.div`
@@ -16,8 +17,8 @@ export const Wrapper = styled.div`
 `;
 
 export const PlusButton = styled.div`
-  width: 102px;
-  height: 102px;
+  min-width: 102px;
+  min-height: 102px;
 
   background: #2af598;
   border-radius: 10px;
